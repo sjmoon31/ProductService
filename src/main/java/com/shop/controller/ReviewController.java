fc477000fc9a08b191df9a127004d5a207d1977a
Moon Seung Ju <sjmoon-725@naver.com>
2024-12-31 14:36:53 +0900

    MSA 쇼핑몰 프로젝트(상품 관리) 미사용 소스 제거_241228

--- src/main/java/com/shop/controller/ReviewController.java
+++ src/main/java/com/shop/controller/ReviewController.java
@@ -7,7 +7,6 @@ import com.shop.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
